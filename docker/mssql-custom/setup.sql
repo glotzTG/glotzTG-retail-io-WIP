bf06e147a19634314f01dad230464e589b1f0785
Gunther <126821798+glotzTG@users.noreply.github.com>
2023-06-30 16:41:23 -0700

    Initial Commit
    Working on Docker SQL

---
+++ docker/mssql-custom/setup.sql
@@ -0,0 +1,19 @@
+-- Declare variables
+DECLARE @DatabaseName NVARCHAR(100) = '$(backupFilename)';
+DECLARE @DataFilePath NVARCHAR(100) = '/var/opt/mssql/data/JE_Empty_20231.mdf';
+DECLARE @LogFilePath NVARCHAR(100) = '/var/opt/mssql/log/JE_Empty_20231.ldf';
+DECLARE @SqlStatement NVARCHAR(MAX);
+
+-- Construct the dynamic SQL statement
+SET @SqlStatement = N'RESTORE DATABASE ' + QUOTENAME(@DatabaseName) + N'
+FROM DISK = ''/var/opt/mssql/backup/' + @DatabaseName + N'.bak'' 
+WITH 
+   MOVE ''JustEnough'' TO ''' + @DataFilePath + N''',
+   MOVE ''JustEnough_log'' TO ''' + @LogFilePath + N''',
+   NOUNLOAD, REPLACE;';
+
+-- Execute the dynamic SQL statement
+PRINT @SqlStatement
+EXEC sp_executesql @SqlStatement;
+
+--RESTORE DATABASE [JE_Empty_20231] FROM DISK = '/var/opt/mssql/backup/JE_Empty_20231.bak' WITH MOVE 'JE_Empty_20231' TO '/var/opt/mssql/data/JE_Empty_20231.mdf', MOVE 'JE_Empty_20231' TO '/var/opt/mssql/log/JE_Empty_20231.ldf', NOUNLOAD, REPLACE
